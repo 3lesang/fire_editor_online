1abb9ff08f763d79f934cb860786f8d65f9b059e
Minh Sang <msang.sg@gmail.com>
2022-12-11 16:30:06 +0700

    fix for deloy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 require('dotenv').config();
 const app = express();
 const route = require('./routes');
@@ -14,6 +15,7 @@ app.use(
     limit: '50mb',
   })
 );
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 route(app);
